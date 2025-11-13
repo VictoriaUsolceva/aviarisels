3c58187d66a0b136bacccfe21c1d47239d95888e
VictoriaUsolceva <nikasidorova039@gmail.com>
2025-11-13 13:42:49 +0300

    добавлена секция napravnelia и внесены прочие дополнения к проекту

--- js/script.js
+++ js/script.js
@@ -5,10 +5,8 @@ document.querySelector(".menu__button")?.addEventListener("click", () => {
 // Получаем элемент, у которого нужно удалить класс
 const myElement = document.querySelector(".menu");
 const classNameToRemove = "menu_opened";
-
 // Создаем медиа-запрос для размера экрана
 const mediaQuery = window.matchMedia("(min-width: 768px)"); // Укажите свой размер (например, 768px)
-
 // Функция, которая будет удалять класс
 function removeClassOnSizeChange(e) {
   if (e.matches) {
@@ -16,18 +14,52 @@ function removeClassOnSizeChange(e) {
     myElement.classList.remove(classNameToRemove);
   }
 }
-
 removeClassOnSizeChange(mediaQuery);
-
 mediaQuery.addEventListener("change", removeClassOnSizeChange);
 
 // функция чтобы узнать ширину html без прокрутки
 const html = document.querySelector("html");
-
 function updateWidth() {
   const offsetWidth = html.offsetWidth;
   html.style.setProperty("--window-width", `${offsetWidth}px`);
 }
-
 window.addEventListener("resize", updateWidth);
 updateWidth();
+
+// работа кнопок в секции napravnelia
+
+// // Получаем ссылку на кнопку
+// const toColombo = document.getElementById("toColombo");
+// // Получаем ссылку на input
+// const inputWhere = document.getElementById("where");
+
+// // Добавляем обработчик события клика
+// toColombo.addEventListener("click", function () {
+//   // Подставляем новое значение в value input
+//   inputWhere.value = "Шри-Ланка";
+//   console.log(inputWhere, toColombo)
+//   window.scrollTo(0, 0)
+// });
+
+// Получаем ссылку на контейнер списка
+const itemList = document.querySelector(".napravlenia__list");
+// Получаем ссылку на целевой input
+const targetInput = document.getElementById("where");
+
+// Добавляем обработчик события клика к родительскому элементу
+itemList.addEventListener("click", function (event) {
+  // Определяем, был ли клик на элементе с классом item
+  let clickedItem = event.target.closest(".napravlenia__list-item");
+
+  if (clickedItem) {
+    // Находим кнопку внутри кликнутого элемента списка
+    const button = clickedItem.querySelector(".napravlenia__button");
+
+    if (button) {
+      const buttonText = button.textContent || button.innerText;
+      const lastWord = buttonText.trim().split(/\s+/)[0] || '';
+      targetInput.value = lastWord;
+      window.scrollTo(0, 0);
+    }
+  }
+});
